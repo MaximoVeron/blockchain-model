ca71c676d7b2e352977e1cb2e18e553d349a66af
MaximoVeron <maximoveron49@gmail.com>
2026-04-24 13:50:09 -0300

    correccion del servidor p2p

--- app/p2p.server.js
+++ app/p2p.server.js
@@ -1,6 +1,6 @@
-import { WebSocketServer, WebSocket } from 'ws';
-import { BlockChain } from '../blockchain/blockChain.js';
-const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+import { WebSocketServer, WebSocket } from "ws";
+import { BlockChain } from "../blockchain/blockChain.js";
+const peers = process.env.PEERS ? process.env.PEERS.split(",") : [];
 const p2pPORT = process.env.P2P_PORT || 5001;
 
 export class p2pServer {
@@ -11,7 +11,7 @@ export class p2pServer {
   listen() {
     const server = new WebSocketServer({ port: p2pPORT });
 
-    server.on('connection', (socket) => {
+    server.on("connection", (socket) => {
       this.connectSocket(socket);
     });
     this.connectToPeers();
@@ -21,25 +21,24 @@ export class p2pServer {
   connectToPeers() {
     peers.forEach((peer) => {
       const socket = new WebSocket(peer);
-      socket.on('open', () => {
+      socket.on("open", () => {
         this.connectSocket(socket);
       });
 
       // Es bueno añadir manejo de errores para que no crashé el app si un peer está caído
-      socket.on('error', (err) => console.log(`Connection to ${peer} failed.`));
+      socket.on("error", (err) => console.log(`Connection to ${peer} failed.`));
     });
   }
 
   connectSocket(socket) {
     this.sockets.push(socket);
-    console.log('[+]socket connected');
     this.messageHandler(socket);
-    this.sendChain();
+    this.sendChain(socket);
   }
 
   messageHandler(socket) {
-    socket.on('message', (message) => {
-      const data = JSON.parse(message);
+    socket.on("message", (message) => {
+      const data = JSON.parse(message.toString()); // Convertir a string explícitamente
       this.BlockChain.replaceChain(data);
     });
   }
